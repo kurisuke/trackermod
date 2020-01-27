10be2eb9d2315247ad320301ade02588946ce81e
Peter Helbing <peter@abulafia.org>
2020-01-27 19:24:39 +0100

    protracker: effects (WIP)

--- src/player/protracker/mod.rs
+++ src/player/protracker/mod.rs
@@ -1,5 +1,5 @@
 use super::{InitError, OutputFormat, PlayError, SampleBuffer, SampleFormat, SampleOutput};
-use crate::format::protracker::ProtrackerMod;
+use crate::format::protracker::{Effect, EffectType, EffectTypeExtended, ProtrackerMod};
 
 pub struct ProtrackerPlayer {
     pt_mod: ProtrackerMod,
@@ -30,8 +30,11 @@ struct PlayerState {
 struct ChannelState {
     pub sample_no: Option<u8>,
     pub volume: u16,
+    pub volume_diff: i16,
     pub offset: f64,
-    pub advance: f64,
+    pub period: u16,
+    pub period_diff: i16,
+    pub period_target: Option<u16>,
     pub in_loop: bool,
 }
 
@@ -50,13 +53,16 @@ impl ProtrackerPlayer {
                 SampleFormat::F32 => SampleBuffer::F32(vec![]),
             };
             let num_channels = pt_mod.patterns[0].divisions[0].channel_data.len();
-            Ok(ProtrackerPlayer {
+            let start_pattern = pt_mod.sequence[0];
+            let mut player = ProtrackerPlayer {
                 pt_mod,
                 clock_freq,
                 output_format,
                 state: PlayerState::default(num_channels),
                 buffer,
-            })
+            };
+            player.state.cur_pattern = start_pattern as usize;
+            Ok(player)
         }
     }
 
@@ -84,7 +90,7 @@ impl ProtrackerPlayer {
             // new division
 
             // parse notes & effects and set parameters
-            self.process_new_division();
+            self.update_division();
 
             let samples_per_tick = (self.output_format.sample_rate as f32 * 60.0
                 / self.state.ticks_per_min as f32)
@@ -110,6 +116,7 @@ impl ProtrackerPlayer {
                 }
             };
         }
+        self.update_tick();
 
         self.calc_output_samples();
 
@@ -131,6 +138,8 @@ impl ProtrackerPlayer {
     }
 
     fn calc_output_samples(&mut self) {
+        let advances = self.calc_advances();
+
         let samples_per_tick = (self.output_format.sample_rate as f32 * 60.0
             / self.state.ticks_per_min as f32)
             .floor() as usize;
@@ -139,7 +148,7 @@ impl ProtrackerPlayer {
                 let mut val = 0.0;
                 let num_input_channels = self.state.channels.len();
                 for c in 0..num_input_channels {
-                    val += self.next_sample(c);
+                    val += self.next_sample(c, advances[c]);
                 }
                 val /= (num_input_channels / 2) as f64;
 
@@ -156,9 +165,9 @@ impl ProtrackerPlayer {
                 let num_input_channels = self.state.channels.len();
                 for c in 0..num_input_channels {
                     if c % 4 == 0 || c % 4 == 3 {
-                        val_l += self.next_sample(c);
+                        val_l += self.next_sample(c, advances[c]);
                     } else {
-                        val_r += self.next_sample(c);
+                        val_r += self.next_sample(c, advances[c]);
                     }
                     val_l /= (num_input_channels / 4) as f64;
                     val_r /= (num_input_channels / 4) as f64;
@@ -183,7 +192,7 @@ impl ProtrackerPlayer {
         }
     }
 
-    fn next_sample(&mut self, channel_no: usize) -> f64 {
+    fn next_sample(&mut self, channel_no: usize, advance: f64) -> f64 {
         let channel = &mut self.state.channels[channel_no];
 
         if channel.sample_no.is_none() {
@@ -199,7 +208,7 @@ impl ProtrackerPlayer {
             0.0
         };
 
-        channel.offset += channel.advance;
+        channel.offset += advance;
 
         if sample.repeat_length > 2 {
             // can loop
@@ -226,23 +235,101 @@ impl ProtrackerPlayer {
         val
     }
 
-    fn process_new_division(&mut self) {
+    fn update_division(&mut self) {
         let pattern = &self.pt_mod.patterns[self.state.cur_pattern];
         let division = &pattern.divisions[self.state.cur_division];
 
         for (idx, cd) in division.channel_data.iter().enumerate() {
             if cd.sample > 0 {
                 // new note
-                let cf = match self.clock_freq {
-                    ClockFreq::Pal => CLOCK_FREQ_PAL,
-                    ClockFreq::Ntsc => CLOCK_FREQ_NTSC,
-                };
-                let samples_per_sec = cf / cd.period as f64;
-                let advance = samples_per_sec / (self.output_format.sample_rate * 2) as f64;
+                let cs = &mut self.state.channels[idx];
                 let vol = self.pt_mod.samples[cd.sample as usize - 1].volume as u16;
-                self.state.channels[idx].reset(cd.sample, vol, advance);
+
+                // handle effects
+                match cd.effect {
+                    Effect::Normal {
+                        effect_type,
+                        param1,
+                        param2,
+                    } => {
+                        match effect_type {
+                            EffectType::SlideUp => {
+                                cs.reset(cd.sample, vol, cd.period);
+                                cs.period_diff = (param1 * 16 + param2) as i16;
+                            }
+                            EffectType::SlideDown => {
+                                cs.reset(cd.sample, vol, cd.period);
+                                cs.period_diff = 0 - (param1 * 16 + param2) as i16;
+                            }
+                            EffectType::SlideToNote => {
+                                // do not set the new note directly, keep the last one
+                                cs.period_target = Some(cd.period);
+                                if cd.period < cs.period {
+                                    cs.period_diff = 0 - (param1 * 16 + param2) as i16;
+                                } else {
+                                    cs.period_diff = (param1 * 16 + param2) as i16;
+                                }
+                            }
+                            EffectType::SetVolume => {
+                                cs.reset(cd.sample, vol, cd.period);
+                                cs.volume = (param1 * 16 + param2) as u16;
+                            }
+                            EffectType::SetSpeed => {
+                                let speed_val = param1 * 16 + param2;
+                                if speed_val == 0 {
+                                    // ignore
+                                } else if speed_val >= 1 && speed_val <= 32 {
+                                    self.state.ticks_per_div = speed_val;
+                                } else {
+                                    self.state.ticks_per_min = 4 * 6 * speed_val as u16;
+                                }
+                            }
+                            _ => {
+                                cs.reset(cd.sample, vol, cd.period);
+                            }
+                        }
+                    }
+                    Effect::Extended {
+                        effect_type: _,
+                        param: _,
+                    } => {
+                        cs.reset(cd.sample, vol, cd.period);
+                    }
+                }
             }
         }
+    }
+
+    fn update_tick(&mut self) {
+        for channel in self.state.channels.iter_mut() {
+            match channel.period_target {
+                Some(period_target) => {
+                    if channel.period_diff < 0 {
+                        channel.period = ((channel.period as i16 + channel.period_diff) as u16)
+                            .max(period_target);
+                    } else {
+                        channel.period = ((channel.period as i16 + channel.period_diff) as u16)
+                            .min(period_target);
+                    }
+                }
+                None => {
+                    channel.period = (channel.period as i16 + channel.period_diff) as u16;
+                }
+            }
+        }
+    }
+
+    fn calc_advances(&self) -> Vec<f64> {
+        let mut advances = vec![];
+        for channel in self.state.channels.iter() {
+            let cf = match self.clock_freq {
+                ClockFreq::Pal => CLOCK_FREQ_PAL,
+                ClockFreq::Ntsc => CLOCK_FREQ_NTSC,
+            };
+            let samples_per_sec = cf / channel.period as f64;
+            advances.push(samples_per_sec / (self.output_format.sample_rate * 2) as f64);
+        }
+        advances
     }
 }
 
@@ -267,17 +354,23 @@ impl ChannelState {
         ChannelState {
             sample_no: None,
             volume: 0,
+            volume_diff: 0,
             offset: 0.0,
-            advance: 0.0,
+            period: 0,
+            period_diff: 0,
+            period_target: None,
             in_loop: false,
         }
     }
 
-    fn reset(&mut self, sample_no: u8, volume: u16, advance: f64) {
+    fn reset(&mut self, sample_no: u8, volume: u16, period: u16) {
         self.sample_no = Some(sample_no);
         self.volume = volume;
+        self.volume_diff = 0;
         self.offset = 0.0;
-        self.advance = advance;
+        self.period = period;
+        self.period_diff = 0;
+        self.period_target = None;
         self.in_loop = false;
     }
 }
